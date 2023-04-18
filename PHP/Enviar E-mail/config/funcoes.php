f0b8e4ae95ebaa1cc0bb242b3593b13c38505be4
João Vitor <52049527+JoaoVitor2344@users.noreply.github.com>
2023-04-18 08:52:37 -0300

    Create funcoes.php

---
+++ PHP/Enviar E-mail/config/funcoes.php
@@ -0,0 +1,29 @@
+<?php 
+
+function sendMail($nomeDestino, $emailDestino, $assunto, $msg, PDO $conn, $arquivo = "") {
+  $mail = new PHPMailer(true);
+  $mail->isSMTP();
+  $mail->SMTPDebug   = 1;
+  $mail->SMTPSecure = 'ssl';
+  $mail->Debugoutput = 'html';
+  $mail->Host        = getStatic($conn, 'site_mail_host');
+  $mail->Port        = getStatic($conn, 'site_mail_porta');
+  $mail->SMTPAuth    = true;
+  $mail->Username    = getStatic($conn, 'site_mail_user'); 
+  $mail->Password    = getStatic($conn, 'site_mail_pass');
+  $mail->SetFrom(getStatic($conn, 'site_mail_user'), getStatic($conn, 'site_mail_exibicao'));
+  $mail->AddAddress($emailDestino, utf8_decode($nomeDestino));
+  $mail->Subject = utf8_decode($assunto);
+  $mail->msgHTML(utf8_decode($msg));
+  if($arquivo == true)
+    $mail->AddAttachment($arquivo);
+
+  if(!$mail->send()){
+    saveLog($conn, "Falha envio de e-mail: ".$mail->ErrorInfo);
+    return false;
+  }else{
+    return true;
+  }
+}
+
+?>
