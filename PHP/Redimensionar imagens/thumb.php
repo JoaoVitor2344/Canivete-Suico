86b364a670f9d5335282def3c4739416f1ee669d
João Vitor <52049527+JoaoVitor2344@users.noreply.github.com>
2023-04-19 13:32:49 -0300

    Update thumb.php

--- PHP/Redimensionar imagens/thumb.php
+++ PHP/Redimensionar imagens/thumb.php
@@ -1,31 +1,48 @@
 <?php
+error_reporting(E_ALL);
+
 try {
     // Caminho da imagem original
     $filename = "https://fomentotech.com.br/".$_GET["img"];
     
-    // Criando uma nova instância do Imagick
-    $imagick = new \Imagick($filename);
-    
-    // Obtendo a largura e a altura da imagem original
-    $width = $imagick->getImageWidth();
-    $height = $imagick->getImageHeight();
+    // Obtendo as dimensões da imagem original
+    list($originalWidth, $originalHeight) = getimagesize($filename);
     
     // Definindo a largura e a altura máximas para a imagem redimensionada
-    $newWidth = $_GET['width'];
-    $newHeight = $_GET['height'];
+    $newWidth = (isset($_GET['width'])) ? ($_GET['width'] * 70) / 100 : $originalWidth;
+    $newHeight =(isset($_GET['height'])) ? ($_GET['height'] * 70) / 100 : $originalHeight;
     
-    // Redimensionando a imagem mantendo a proporção
-    $imagick->scaleImage($newWidth, $newHeight, true);
+    // Calculando as novas dimensões mantendo a proporção
+    if ($originalWidth > $originalHeight) {
+       $newHeight = ceil($originalHeight * ($newWidth / $originalWidth));
+    } else {
+       $newWidth = ceil($originalWidth * ($newHeight / $originalHeight));
+    }
     
-    // Obtendo os dados da imagem redimensionada
-    $imageData = $imagick->getImageBlob();
+    // Criando uma nova imagem a partir do arquivo original
+    $extensao = pathinfo($filename, PATHINFO_EXTENSION);
+    switch ($extensao) {
+       case 'jpg':
+       case 'jpeg':
+          $imagemOriginal = imagecreatefromjpeg($filename);
+          break;
+       case 'png':
+          $imagemOriginal = imagecreatefrompng($filename);
+          break;
+    }
     
-    // Gerando a imagem temporária
-    header('Content-Type: image/jpeg');
-    echo $imageData;
+    // Criando uma nova imagem redimensionada
+    $imagemRedimensionada = imagecreatetruecolor($newWidth, $newHeight);
+    imagecopyresampled($imagemRedimensionada, $imagemOriginal, 0, 0, 0, 0, $newWidth, $newHeight, $originalWidth, $originalHeight);
+    
+    // Convertendo a imagem redimensionada para o formato WebP
+    header('Content-Type: image/webp');
+    imagewebp($imagemRedimensionada);
     
     // Liberando a memória
-    $imagick->destroy();
+    imagedestroy($imagemOriginal);
+    imagedestroy($imagemRedimensionada);
+
 } catch (Exception $e) {
     echo $e->getMessage();
 }
