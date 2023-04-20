c343f12cbb550b9ad4ee585ddc50b31a6f5b51ef
João Vitor <52049527+JoaoVitor2344@users.noreply.github.com>
2023-04-20 09:44:37 -0300

    Create index2.php

---
+++ PHP/LGPD/index2.php
@@ -0,0 +1,22 @@
+<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/cookieconsent@3/build/cookieconsent.min.css" />
+
+<script src="https://cdn.jsdelivr.net/npm/cookieconsent@3/build/cookieconsent.min.js" data-cfasync="false"></script>
+<script>
+    window.cookieconsent.initialise({
+      "palette": {
+        "popup": {
+          "background": "#000"
+        },
+        "button": {
+          "background": "#f1d600"
+        }
+      },
+      "position": "bottom-left",
+      "content": {
+        "message": "Este site utiliza cookies para garantir uma melhor experiência do usuário. Ao continuar navegando neste site, você concorda com o uso de cookies. Para mais informações, consulte nossa política de privacidade.",
+        "dismiss": "Aceitar",
+        "link": "Saiba mais",
+        "href": "https://lojasandalmaq.com.br/politica-de-privacidade"
+      }
+    });
+</script>
